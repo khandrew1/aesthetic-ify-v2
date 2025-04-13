6a4a24d3e600f3fe33729236dde7a7a4387be58d
Andrew Khadder <akhad007@ucr.edu>
2025-04-12 17:00:55 -0700

    add next auth and login
    allows for spotify OAuth login

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,14 +1,24 @@
 import { Button } from "./ui/button";
 import { BsSpotify } from "react-icons/bs";
 
+import { signIn } from "@/auth";
+
 const Landing = () => {
 	return (
 		<div className="flex flex-col items-center justify-center w-full gap-y-4 h-full">
-			<p className="text-3xl text-white italic font-bold">aesthetic-ify v2</p>
-			<Button className="flex hover:cursor-pointer">
-				<BsSpotify className="text-9xl" />
-				Log in with Spotify
-			</Button>
+			<p className="text-3xl italic font-bold">aesthetic-ify</p>
+			<form
+				action={async () => {
+					"use server";
+					await signIn("spotify");
+				}}
+			>
+				<Button type="submit" className="flex hover:cursor-pointer">
+					<BsSpotify className="text-9xl" />
+					Log in with Spotify
+				</Button>
+			</form>
+			<p className="text-xs italic font-semibold">v2.0.0</p>
 		</div>
 	);
 };
